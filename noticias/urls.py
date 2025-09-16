b128e1a000fddbc8a8c2a185764129b4b8decea1
Davi Lucas <davileite@ads.fiponline.edu.br>
2025-09-15 22:24:42 -0300

    Delete noticias directory

--- noticias/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_noticias, name='lista_noticias'),
-    path('noticia/<int:noticia_id>/', views.detalhe_noticia, name='detalhe_noticia'),
-]
-
