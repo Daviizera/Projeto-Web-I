e221c4b697aa06cf602d48868bfc956e98ab0dda
Davi Lucas <davileite@ads.fiponline.edu.br>
2025-09-15 22:23:09 -0300

    Delete noticias directory

--- noticias/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .models import ConfiguracaoSite
-
-def site_config(request):
-    try:
-        config = ConfiguracaoSite.objects.get(pk=1)
-        return {'site_config': config}
-    except ConfiguracaoSite.DoesNotExist:
-        return {'site_config': None}
